da833a0171347aeed6783145f5efde3a02eaf3ea
Cleber Gasparoto <chgasparoto@gmail.com>
2021-04-17 06:19:20 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "curso-aws-com-terraform"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "gha-terraform"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing! And tha's amazing, isn't it?"
+  }
+}
