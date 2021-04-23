976be005a3cd6cde8d56b3ff2c182761f7f24a34
Cleber Gasparoto <chgasparoto@gmail.com>
2021-04-23 06:11:04 +0200

    Feat: S3 remote state configuration (#2)
    * add s3 remote state
    
    * add backend config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,34 @@
-terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "curso-aws-com-terraform"
+//terraform {
+//  backend "remote" {
+//    # The name of your Terraform Cloud organization.
+//    organization = "curso-aws-com-terraform"
+//
+//    # The name of the Terraform Cloud workspace to store Terraform state files in.
+//    workspaces {
+//      name = "gha-terraform"
+//    }
+//  }
+//}
 
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "gha-terraform"
+terraform {
+  required_version = "0.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.32.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
     }
   }
+
+  backend "s3" {}
 }
 
 provider "aws" {
   region = "eu-central-1"
-}
-
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing! And that's amazing, isn't it? It has run on ${timestamp()}"
-  }
 }
 
 resource "random_pet" "this" {
